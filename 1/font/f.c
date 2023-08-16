5a326daf48a815ab9e88a0586ac6d6aaa08d51a6
orlive <oli@orlive.de>
2023-08-16 23:25:25 +0200

    (-) make program compilable

--- 1/font/f.c
+++ 1/font/f.c
@@ -2,238 +2,236 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <curses.h>
+#include <string.h>
 #include "f.h"
 
 WINDOW *hw;
 
-char	datei[256];
+char  datei[256];
 
-unsigned char	font[4096];
-unsigned char	bit[8] = { 128,64,32,16,8,4,2,1 };
+unsigned char  font[4096];
+unsigned char  bit[8] = { 128,64,32,16,8,4,2,1 };
 
-main(int argc,char **argv)
+int main(int argc,char **argv)
 {
-	int	i,u;
+  int  i,u;
 
-	if( argc==1 )
-		exit(1);
+  if ( argc==1 )
+    exit(1);
 
-	strcpy(datei,argv[1]);
+  strcpy(datei,argv[1]);
 
-	FontInit();
+  FontInit();
 
-	FontHaupt();
+  FontHaupt();
 
-	FontUninit();
+  FontUninit();
+
+  return 0;
 }
 
 void FontInit(void)
 {
-	hw = initscr();
-	(void) noecho();
-	(void) keypad(hw,TRUE);
+  hw = initscr();
+  (void) noecho();
+  (void) keypad(hw,TRUE);
 }
 
 void FontUninit(void)
 {
-	(void) echo();
-	(void) endwin();
-	exit(1);
+  (void) echo();
+  (void) endwin();
+  exit(1);
 }
 
 void FontHaupt(void)
 {
-	FontLaden();
+  FontLaden();
 
-	FontVidi();
+  FontVidi();
 
-	FontDrawAll();
+  FontDrawAll();
 
-	FontWahl();
+  FontWahl();
 
-	FontSpeichern();
+  FontSpeichern();
 }
 
 void FontWahl(void)
 {
-	int	x,y;
-	int	t;
+  int  x,y;
+  int  t;
 
-	x = y = 0;
+  x = y = 0;
 
-	do{
+  do{
 
-		if( x<0 ) x=15;
-		if( x>15) x=0;
-		if( y<0 ) y=15;
-		if( y>15) y=0;
+    if ( x<0 ) x=15;
+    if ( x>15) x=0;
+    if ( y<0 ) y=15;
+    if ( y>15) y=0;
 
-		FontDrawOne(y*16+x);
+    FontDrawOne(y*16+x);
 
-		mvwaddch( hw , y + 1 , x*2 + 20 , '(' );
-		mvwaddch( hw , y + 1 , x*2 + 22 , ')' );
+    mvwaddch( hw , y + 1 , x*2 + 20 , '(' );
+    mvwaddch( hw , y + 1 , x*2 + 22 , ')' );
 
-		t = getch();
+    t = getch();
 
-		mvwaddch( hw , y + 1 , x*2 + 20 , ' ' );
-		mvwaddch( hw , y + 1 , x*2 + 22 , ' ' );
+    mvwaddch( hw , y + 1 , x*2 + 20 , ' ' );
+    mvwaddch( hw , y + 1 , x*2 + 22 , ' ' );
 
-		switch(t)
-		{
-			case KEY_DOWN:
-				y++;
-				break;
-			case KEY_UP:
-				y--;
-				break;
-			case KEY_LEFT:
-				x--;
-				break;
-			case KEY_RIGHT:
-				x++;
-				break;
-			case '\n':
-				FontEdit(y*16+x);
-				FontSpeichern();
-				FontVidi();
-				break;
-				break;
-		}
+    switch (t) {
+      case KEY_DOWN:
+        y++;
+        break;
+      case KEY_UP:
+        y--;
+        break;
+      case KEY_LEFT:
+        x--;
+        break;
+      case KEY_RIGHT:
+        x++;
+        break;
+      case '\n':
+        FontEdit(y*16+x);
+        FontSpeichern();
+        FontVidi();
+        break;
+        break;
+    }
 
-	} while( t!=27 );
-	
+  } while ( t!=27 );
+  
 }
 
 void FontEdit(int nr)
 {
-	int	x,y;
-	int	t;
-	unsigned char	a;
+  int  x,y;
+  int  t;
+  unsigned char  a;
 
-	x = y = 0;
+  x = y = 0;
 
-	do{
+  do{
 
-		if( x<0 ) x=7;
-		if( x>7 ) x=0;
-		if( y<0 ) y=15;
-		if( y>15) y=0;
+    if ( x<0 ) x=7;
+    if ( x>7 ) x=0;
+    if ( y<0 ) y=15;
+    if ( y>15) y=0;
 
-		a = font[nr*16+y];
-		wmove(hw,y+1,x+1);
+    a = font[nr*16+y];
+    wmove(hw,y+1,x+1);
 
-		if( (a & bit[x]) == bit[x] )
-			waddch( hw,'*' );
-		else
-			waddch( hw,'.' );
+    if ( (a & bit[x]) == bit[x] )
+      waddch( hw,'*' );
+    else
+      waddch( hw,'.' );
 
-		wmove(hw,y+1,x+1);
+    wmove(hw,y+1,x+1);
 
-		t = getch();
+    t = getch();
 
-		switch(t)
-		{
-			case KEY_DOWN:
-				y++;
-				break;
-			case KEY_UP:
-				y--;
-				break;
-			case KEY_LEFT:
-				x--;
-				break;
-			case KEY_RIGHT:
-				x++;
-				break;
-			case ' ':
-				a = font[nr*16+y];
-				if( (a & bit[x]) == bit[x] )
-					a = a & ~bit[x];
-				else
-					a = a | bit[x];
-				font[nr*16+y] = a;
-				break;
-		}
+    switch (t) {
+      case KEY_DOWN:
+        y++;
+        break;
+      case KEY_UP:
+        y--;
+        break;
+      case KEY_LEFT:
+        x--;
+        break;
+      case KEY_RIGHT:
+        x++;
+        break;
+      case ' ':
+        a = font[nr*16+y];
+        if ( (a & bit[x]) == bit[x] )
+          a = a & ~bit[x];
+        else
+          a = a | bit[x];
+        font[nr*16+y] = a;
+        break;
+    }
 
-	} while( t!='\n' );
-	
+  } while ( t!='\n' );
+  
 }
 
 void FontLaden(void)
 {
-	FILE	*d;
+  FILE  *d;
 
-	if( !(d=fopen(datei,"r")) )
-		FontExit("Kann Datei nicht lesen");
+  if ( !(d=fopen(datei,"r")) )
+    FontExit("Kann Datei nicht lesen");
 
-	if( fread(font,1,4096,d)!=4096 )
-		FontExit("Fehler beim Lesen");
+  if ( fread(font,1,4096,d)!=4096 )
+    FontExit("Fehler beim Lesen");
 
-	fclose(d);
+  fclose(d);
 }
 
 void FontSpeichern(void)
 {
-	FILE	*d;
+  FILE  *d;
 
-	if( !(d=fopen(datei,"w")) )
-		FontExit("Kann Datei nicht Schreiben");
+  if ( !(d=fopen(datei,"w")) )
+    FontExit("Kann Datei nicht Schreiben");
 
-	if( fwrite(font,1,4096,d)!=4096 )
-		FontExit("Fehler beim Schreiben");
+  if ( fwrite(font,1,4096,d)!=4096 )
+    FontExit("Fehler beim Schreiben");
 
-	fclose(d);
+  fclose(d);
 }
 
 void FontDrawOne(int nr)
 {
-	int	u,i,t;
-	unsigned char	a;
+  int  u,i,t;
+  unsigned char  a;
 
-	for( i=0 ; i<16 ; i++ )
-	{
-		a = font[nr*16+i];
-		wmove(hw,i+1,1);
-		for( t=0 ; t<8 ; t++ )
-			if( (a & bit[t]) == bit[t] )
-				waddch( hw,'*' );
-			else
-				waddch( hw,'.' );
-	}
+  for ( i=0 ; i<16 ; i++ ) {
+    a = font[nr*16+i];
+    wmove(hw,i+1,1);
+    for ( t=0 ; t<8 ; t++ )
+      if ( (a & bit[t]) == bit[t] )
+        waddch( hw,'*' );
+      else
+        waddch( hw,'.' );
+  }
 }
 
 void FontDrawAll(void)
 {
-	int	i,x,y;
+  int  i,x,y;
 
-	x = 1;
-	y = 1;
+  x = 1;
+  y = 1;
 
-	for( i=0 ; i<256 ; i++ )
-	{
-		wmove(hw,y,2*x+19);
+  for ( i=0 ; i<256 ; i++ ) {
+    wmove(hw,y,2*x+19);
 
-		waddch( hw,i );
-		 
-		if(++x > 16)
-		{
-			y++;
-			x=1;
-		}
-	}
+    waddch( hw,i );
+     
+    if (++x > 16) {
+      y++;
+      x=1;
+    }
+  }
 }
 
 void FontExit(char *str)
 {
-	fprintf(stderr,"%s\n",str);
-	FontUninit();
+  fprintf(stderr,"%s\n",str);
+  FontUninit();
 }
 
 void FontVidi(void)
 {
-	char	tmp[500];
+  char  tmp[500];
 
-	sprintf(tmp,"vidi -f %s font8x16",datei);
+  sprintf(tmp,"vidi -f %s font8x16",datei);
 
-	(void)system(tmp);
+  (void)system(tmp);
 }
